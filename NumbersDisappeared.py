e8942c84317a35ee9530ca7da867392a361a1ea8
Shamima <shamima.hossain19@gmail.com>
2021-11-20 20:04:45 +0600

    Add files via upload

--- NumbersDisappeared.py
+++ NumbersDisappeared.py
@@ -6,3 +6,15 @@ class Solution:
         for i in range(1, len(nums)):
             set2.add(i)
         return set2.difference(set1)
+    ### OR ##
+
+    def findDisappearedNumbers(self, nums):
+        result = []
+        for i in range(len(nums)):
+            idx = abs(nums[i])-1
+            if nums[idx] > 0:
+                nums[idx] = -nums[idx]
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i+1)
+        return result
